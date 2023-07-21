9b2b6674c649d5b0406bd56555b8be4a55f52c9d
Avinandan Roy <134713103+AvinandanRoy@users.noreply.github.com>
2023-07-21 22:28:58 +0600

    Add files via upload

---
+++ bee1116.c
@@ -0,0 +1,30 @@
+/*
+Write a program that read two numbers X and Y and print the result of dividing the X by Y. If it's not possible, print the message "divisao impossivel".
+
+Input
+The input contains an integer number N. This N is the quantity of pairs of integer numbers X and Y read (dividend and divisor).
+
+Output
+For each test case print the result of this division with one digit after the decimal point, or “divisao impossivel” if it isn't possible to perform the calculation.
+
+Obs.: Be carefull. The division between two integers in some languages generates another integer. Use cast:)
+*/
+
+#include<stdio.h>
+int main()
+{
+    int n ,i ;
+    float x , y ;
+
+    scanf("%d",&n);
+
+    for( i = 0 ; i < n ; i++){
+            scanf("%f%f",&x,&y);
+        if(y == 0){
+            printf("divisao impossivel\n");
+        }
+        else{
+            printf("%.1f\n", (x/ y));
+        }
+    }
+}
